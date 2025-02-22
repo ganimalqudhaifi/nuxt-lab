faf74236a6931534504c301c8388548a6a5e2d41
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2025-02-23 02:17:37 +0700

    Add pages

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <header>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/posts/1">Posts 1</NuxtLink></li>
+          <li><NuxtLink to="/posts/2">Posts 2</NuxtLink></li>
+        </ul>
+      </header>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const route = useRoute();
+
+console.log(route.params.id);
+</script>
+
+<template>
+  <div>posts id: {{ $route.params.id }}</div>
+</template>
