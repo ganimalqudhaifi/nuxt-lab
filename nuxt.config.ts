7df9071e0c6081f650bde11449379f19c6935dab
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2025-02-23 23:24:27 +0700

    Add meta tages on nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Nuxt Lab',
+      meta: [
+        {
+          name: 'description',
+          content: 'This is a repository for experimenting with Nuxt.js'
+        }
+      ]
+    }
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/content', '@pinia/nuxt']
