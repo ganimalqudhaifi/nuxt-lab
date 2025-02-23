06a03d0fa92478f1fd575c88ee221fdc7bcab453
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2025-02-23 19:13:12 +0700

    Add modules

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 const { $sayHello } = useNuxtApp();
+const { data: home } = await useAsyncData(() =>
+  queryCollection("content").path("/").first()
+);
 
 $sayHello("User");
 </script>
@@ -7,5 +10,7 @@ $sayHello("User");
 <template>
   <div>
     <h1>This is a home page</h1>
+    <ContentRenderer v-if="home" :value="home" />
+    <div v-else>Home not found</div>
   </div>
 </template>
