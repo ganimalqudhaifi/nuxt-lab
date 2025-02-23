11d276fba730529e945e65c66d8cd7070d8b74de
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2025-02-23 14:07:18 +0700

    Add composables

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -5,6 +5,7 @@
       <li><NuxtLink to="/about">About</NuxtLink></li>
       <li><NuxtLink to="/posts/1">Posts 1</NuxtLink></li>
       <li><NuxtLink to="/posts/2">Posts 2</NuxtLink></li>
+      <li><NuxtLink to="/events">Events</NuxtLink></li>
     </ul>
   </header>
 </template>
---
+++ pages/events.vue
@@ -0,0 +1,7 @@
+<script setup>
+const mouse = reactive(useMouse());
+</script>
+
+<template>
+  <div>Mouse position is at: {{ mouse.x }} {{ mouse.y }}</div>
+</template>
